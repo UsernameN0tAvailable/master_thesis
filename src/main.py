6f61a69f5d1ab16a8a6202889574b90799e6906e
UsernameN0tAvailable <mattia.pedrazzi@outlook.com>
2023-05-24 14:17:10 +0200

    pipeline ready

---
+++ src/main.py
@@ -0,0 +1,165 @@
+from vit import DinoFeatureClassifier
+import os
+import json
+import torch
+from torch import nn
+from torch.utils.data import Subset, DataLoader, TensorDataset
+from torchvision import transforms
+from PIL import Image
+import argparse
+
+from typing import Optional;
+
+
+def get_optimizer(model, id: Optional[int]):
+    if id is None:
+        raise ValueError("Should specify an optimizer")
+
+    match id:
+        case 0:
+            return torch.optim.SGD(model.params(), lr=0.01)
+        case 1:
+            return torch.optim.RMSprop(model.params(), lr=0.01)
+        case 2: 
+            return torch.optim.Adam(model.params(), lr=0.01)
+        case other:
+            raise ValueError("optimizer value not allowed")
+
+def f1_score(true_labels, predicted_labels):
+    true_positives = (true_labels * predicted_labels).sum().float()
+    false_positives = ((1 - true_labels) * predicted_labels).sum().float()
+    false_negatives = (true_labels * (1 - predicted_labels)).sum().float()
+
+    precision = true_positives / (true_positives + false_positives)
+    recall = true_positives / (true_positives + false_negatives)
+
+    f1 = 2 * ((precision * recall) / (precision + recall))
+    return f1
+
+
+
+
+if __name__ == "__main__":
+
+    # get current split
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--shift', type=int, default=0, help='the shift to apply to the data splits')
+    parser.add_argument('--epochs', type=int, default=100, help='number of epoch')
+    parser.add_argument('--data', type=str, help="data directory")
+    parser.add_argument('--optimizer', type=int, default=0, help="optimizer for the mlp, 0 => SGD, 1 => RMSProp, 2 => Adam")
+
+    args = parser.parse_args()
+
+    # dunno if this check is needed
+    data_dir = args.data
+    if data_dir is None or len(data_dir) == 0:
+        raise ValueError("No data directory given!")
+
+    shift = args.shift
+    if shift is None:
+        raise ValueError("No shift given!")
+
+    num_epochs = args.epochs
+    if num_epochs is None:
+        raise ValueError("No epochs number given!")
+
+
+    splits_dir = data_dir + '/splits.json' 
+
+    # Load the json file
+    with open(splits_dir, "r") as f:
+        data = json.load(f)
+
+    # Define the image transformations
+    transform = transforms.Compose([
+        transforms.Resize((3600, 3600)),  # Resize to 3600x3600 pixels
+        transforms.ToTensor(),  # Convert to PyTorch tensor
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalize to [-1, 1] for RGB
+        ])
+
+    image_dir = data_dir + '/hotspots-png'
+
+    # Load all images into memory and apply transformations
+    all_images = []
+    all_labels = []
+    for split in data.values():
+        for label, images in split.items():
+            for image_name in images:
+                image_path = os.path.join(image_dir, image_name + '.png')
+                image = Image.open(image_path)
+                image = transform(image)
+                all_images.append(image)
+                all_labels.append(int(label))
+
+    # Convert lists to tensors
+    all_images = torch.stack(all_images)
+    all_labels = torch.Tensor(all_labels)
+
+    # Create a Dataset
+    dataset = TensorDataset(all_images, all_labels)
+
+    # Create indices for each split
+    indices = []
+    for split in data.values():
+        split_indices = []
+        for images in split.values():
+            split_indices.extend([image_name for image_name in images])
+        indices.append(split_indices)
+
+    # Number of splits
+    num_splits = len(indices)
+
+    # Init Model Init Model
+    model = DinoFeatureClassifier()  
+
+    # binary output true or false
+    criterion = nn.BCELoss()
+
+    # stochastic gradient descent opt
+    optimizer = get_optimizer(model, args.optimizer)
+
+    # Training and validation loop
+    for epoch in range(num_epochs):
+        # Define training and validation splits for this epoch
+        train_splits = indices[shift: shift + 3]
+        validation_splits = indices[shift + 3: shift + 5]
+
+        # Flatten the lists of splits
+        train_indices = [item for sublist in train_splits for item in sublist]
+        validation_indices = [item for sublist in validation_splits for item in sublist]
+
+        # Create DataLoaders for this epoch
+        train_loader = DataLoader(Subset(dataset, train_indices), batch_size=32, shuffle=True)
+        validation_loader = DataLoader(Subset(dataset, validation_indices), batch_size=32, shuffle=True)
+
+        # Train
+        model.train()  # Set the model to training mode
+        for images, labels in train_loader:
+            optimizer.zero_grad()  # Zero the gradients
+            outputs = model(images)  # Forward pass
+            loss = criterion(outputs, labels)  # Compute the loss
+            loss.backward()  # Compute the gradients
+            optimizer.step()  # Update the weights
+
+        # Validate
+        model.eval()  # Set the model to evaluation mode
+        validation_loss = 0
+        correct = 0
+        all_labels = []
+        all_predictions = []
+        with torch.no_grad():  # Do not compute gradients in this block
+            for images, labels in validation_loader:
+                outputs = model(images)  # Forward pass
+                loss = criterion(outputs, labels)  # Compute the loss
+                validation_loss += loss.item()
+                _, predicted = torch.max(outputs, 1)
+                correct += (predicted == labels).sum().item()
+
+                # Append the labels and predictions for F1 score computation
+                all_labels.extend(labels.tolist())
+                all_predictions.extend(predicted.tolist())
+
+        # Compute the F1 score
+        f1 = f1_score(all_labels, all_predictions)
+
+        print(f'Epoch {epoch + 1}, Train Loss: {loss.item()}, Validation Loss: {validation_loss / len(validation_loader)}, Validation Accuracy: {correct / len(validation_indices)}, Validation F1 Score: {f1}')
---
+++ src/vit.py
@@ -0,0 +1,126 @@
+import torch
+from torch import nn
+import numpy as np
+from torch.nn.functional import normalize
+from typing import Optional
+import re
+
+def make_nonlinear_clusterer(in_channels: int, out_channels: int, bias: Optional[bool] = False):
+    return torch.nn.Sequential(
+        torch.nn.Conv2d(in_channels, in_channels, (1, 1)),
+        torch.nn.ReLU(),
+        torch.nn.Conv2d(in_channels, out_channels, (1, 1), bias=bias)
+    )
+
+def make_linear_clusterer(in_channels: int, out_channels: int, bias: Optional[bool] = False):
+    return torch.nn.Sequential(
+        torch.nn.Conv2d(in_channels, out_channels, (1, 1), bias=bias)
+    )
+
+
+class DinoFeature(nn.Module):
+    def __init__(
+            self,
+            dino_arch: str = "vits16",
+            freeze: str = 'backbone.head',
+    ):
+        super().__init__()
+
+        self.arch = dino_arch
+        self.patch_size = int(re.findall('[0-9][0-9]|[0-9]', self.arch)[0])
+        self.arch = "dino_{}".format(self.arch)
+        self.backbone = torch.hub.load('facebookresearch/dino:main', self.arch)
+        self.n_feats = self.backbone.norm.bias.shape[0]
+
+        self._freeze(freeze=freeze)
+
+    def _freeze(self, freeze: str = 'backbone.head'):
+
+        named_modules = np.array([n for n, _ in self.named_modules()])
+
+        # No freezing option, freeze none
+        if freeze not in named_modules:
+            return
+        else:
+            # Freeze up to layer (not included)
+            id_layer = np.nonzero(named_modules == freeze)[0][0]
+            frozen_layers = named_modules[:id_layer]
+
+            for name, module in self.named_modules():
+                if name in frozen_layers:
+                    # Set the module in evaluation mode
+                    module.eval()
+                    # Disable gradient tracking
+                    module.requires_grad_(False)
+                else:
+                    module.train()
+                    # Enable gradient tracking
+                    module.requires_grad_(True)
+
+    def forward(self, img, n: Optional[int] = 1):
+
+        # Inference for ViT like arch
+        b = img.shape[0]
+        feat_h = img.shape[2] // self.patch_size
+        feat_w = img.shape[3] // self.patch_size
+        # get selected layer activations
+        feat = self.backbone.get_intermediate_layers(x=img, n=n)
+
+        feat_tokens = [f[:, 1:, :].reshape(b, feat_h, feat_w, -1).permute(0, 3, 1, 2) for f in feat]
+        feat_cls_token = [f[:, 0, :] for f in feat]
+
+        if len(feat_tokens) == 1:
+            feat_tokens = feat_tokens[0]
+            feat_cls_token = feat_cls_token[0]
+
+        return feat_cls_token, feat_tokens
+
+
+
+class DinoFeatureClassifier(DinoFeature):
+
+    def __init__(
+            self,
+            dino_arch: str = "vits16",
+            n_cls: int = 2,
+            dropout: float = 0.1,
+            freeze: str = 'backbone.head',
+            type_cls: str = 'nonlinear',
+    ):
+        super(DinoFeatureClassifier, self).__init__(
+            dino_arch=dino_arch, freeze=freeze
+        )
+
+        self.n_cls = n_cls
+        self.dropout = torch.nn.Dropout2d(p=dropout)
+        self.type_cls = type_cls
+
+        if type_cls == 'linear':
+            self.cluster = make_linear_clusterer(
+                in_channels=self.n_feats,
+                out_channels=self.n_cls,
+            )
+        elif type_cls == 'nonlinear':
+            self.cluster = make_nonlinear_clusterer(
+                in_channels=self.n_feats,
+                out_channels=self.n_cls,
+            )
+        else:
+            raise NotImplementedError('Unknown classifier type: {}'.format(type_cls))
+
+    def forward(self, img, n: Optional[int] = 1):
+
+        # Compute embedding
+        z, _ = super(DinoFeatureClassifier, self).forward(img, n=n)
+        z = z.unsqueeze(-1).unsqueeze(-1)
+
+        # Use either cls token or avg of tokens
+        y_pixel = self.dropout(z)
+        y_pixel = self.cluster(y_pixel)
+
+        results = {
+            'z_tokens': z,
+            'y_pixel': y_pixel,
+        }
+
+        return results
