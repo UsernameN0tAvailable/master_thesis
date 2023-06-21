432a4198a5d83f11260284ecea42d55f0d4e1673
UsernameN0tAvailable <mattia.pedrazzi@outlook.com>
2023-06-22 00:04:48 +0200

    actually working

--- src/main.py
+++ src/main.py
@@ -1,182 +1,126 @@
-from vit import DinoFeatureClassifier
 import os
 import json
+import logging
+import argparse
+from typing import List
+import numpy as np
+from PIL import Image
+from sklearn.metrics import f1_score
 import torch
 from torch import nn
-from torch.utils.data import Subset, DataLoader, TensorDataset, Dataset
+from torch.optim import SGD, AdamW, Adam
+from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-from PIL import Image
-import argparse
-from typing import Optional;
+from vit import DinoFeatureClassifier
 
-best_validation_loss = float("inf")
- 
-class CustomImageDataset(Dataset):
-    def __init__(self, image_paths, labels, transform=None):
-        self.image_paths = image_paths
+
+class HotspotDataset(Dataset):
+    def __init__(self, image_dir: str, splits: List[str], labels: List[int], crop_size: int):
+        self.image_dir = image_dir
+        self.splits = splits
         self.labels = labels
-        self.transform = transform
-
+        self.crop_size = crop_size
+        self.transform = transforms.Compose([
+            transforms.Resize((crop_size, crop_size)),
+            transforms.RandomCrop(crop_size),
+            transforms.ToTensor()
+        ])
+        
     def __len__(self):
-        return len(self.image_paths)
+        return len(self.splits)
 
     def __getitem__(self, idx):
-        img_path = self.image_paths[idx]
+        img_name = self.splits[idx]
+        img_path = os.path.join(self.image_dir, f'{img_name}.png')
         label = self.labels[idx]
-        img = Image.open(img_path).convert('RGB')
-
-        if self.transform:
-            img = self.transform(img)
-
-        return img, label
+        image = Image.open(img_path)
+        image = self.transform(image)
+        return image, label
 
 
-def get_optimizer(model, id: Optional[int]):
-    if id is None:
-        raise ValueError("Should specify an optimizer")
-
-    match id:
-        case 0:
-            return torch.optim.SGD(model.parameters(), lr=0.01)
-        case 1:
-            return torch.optim.RMSprop(model.parameters(), lr=0.01)
-        case 2: 
-            return torch.optim.Adam(model.parameters(), lr=0.01)
-        case other:
-            raise ValueError("optimizer value not allowed")
-
-def f1_score(true_labels, predicted_labels):
-    true_positives = (true_labels * predicted_labels).sum().float()
-    false_positives = ((1 - true_labels) * predicted_labels).sum().float()
-    false_negatives = (true_labels * (1 - predicted_labels)).sum().float()
-
-    precision = true_positives / (true_positives + false_positives)
-    recall = true_positives / (true_positives + false_negatives)
-
-    f1 = 2 * ((precision * recall) / (precision + recall))
-    return f1
+def get_data(shift, data_dir, crop_size):
+    with open(os.path.join(data_dir, 'splits.json'), 'r') as f:
+        splits = json.load(f)
+        
+    image_dir = os.path.join(data_dir, 'hotspots-png')
+    
+    image_paths = []
+    labels = []
+    
+    for split in range(5):
+        if (split - shift) % 5 < 3:
+            # Training split
+            for label in ['0', '1']:
+                for img_name in splits[str(split)][label]:
+                    img_path = os.path.join(image_dir, f'{img_name}.png')
+                    if os.path.exists(img_path):
+                        image_paths.append(img_name)
+                        labels.append(int(label))
+                    else:
+                        logging.info(f'Image {img_path} not found.')
+                        
+    return HotspotDataset(image_dir, image_paths, labels, crop_size)
 
 
+def train(model, optimizer, criterion, dataloader, device):
+    model.train()
+    running_loss = 0.0
+    preds = []
+    true = []
+
+    for images, labels in dataloader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        outputs = outputs['y_pixel'].squeeze()
+        labels_one_hot = torch.nn.functional.one_hot(labels, num_classes=2).float()
+
+        loss = criterion(outputs, labels_one_hot)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item() * images.size(0)
+        preds.extend(outputs.detach().cpu().numpy())
+        true.extend(labels.detach().cpu().numpy())
+        
+    epoch_loss = running_loss / len(dataloader.dataset)
+    f1 = f1_score(true, np.round(preds))
+
+    return epoch_loss, f1
 
 
-if __name__ == "__main__":
-
-    # get current split
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--shift', type=int, default=0, help='the shift to apply to the data splits')
-    parser.add_argument('--epochs', type=int, default=100, help='number of epoch')
-    parser.add_argument('--data', type=str, help="data directory")
-    parser.add_argument('--optimizer', type=int, default=0, help="optimizer for the mlp, 0 => SGD, 1 => RMSProp, 2 => Adam")
+    parser.add_argument("--shift", type=int, default=0, choices=[0, 1, 2, 3, 4])
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--data_dir", type=str, default='data')
+    parser.add_argument("--optimizer_index", type=int, default=0, choices=[0, 1, 2])
+    parser.add_argument("--crop_size", type=int, default=128)
 
     args = parser.parse_args()
 
-    # dunno if this check is needed
-    data_dir = args.data
-    if data_dir is None or len(data_dir) == 0:
-        raise ValueError("No data directory given!")
-
-    shift = args.shift
-    if shift is None:
-        raise ValueError("No shift given!")
-
-    num_epochs = args.epochs
-    if num_epochs is None:
-        raise ValueError("No epochs number given!")
-
-
-    splits_dir = data_dir + '/splits.json' 
-
-    # Load the json file
-    with open(splits_dir, "r") as f:
-        data = json.load(f)
-
-    # Define the image transformations
-
-    image_dir = data_dir + '/hotspots-png'
-
-    print("loading images...")
-
-    # Load all images into memory and apply transformations
-    all_images_paths = []
-    all_labels = []
-    for split in data.values():
-        for label, images in split.items():
-            for image_name in images:
-                image_path = os.path.join(image_dir, image_name + '.png')
-                if os.path.isfile(image_path):
-                    all_images_paths.append(image_path)
-                    all_labels.append(int(label))
-                else:
-                    print(f'{image_path} Not Found')
-
-    transform = transforms.Compose([
-        transforms.Resize((3584, 3584)),  # vits16 requires multiples of 16 
-        transforms.ToTensor() 
-        ])
-
-    dataset = CustomImageDataset(all_images_paths, all_labels, transform=transform)
-
-    # Set the indices for the train and validation sets
-    train_indices = [idx for idx in range(len(dataset)) if idx % 5 != shift]
-    validation_indices = [idx for idx in range(len(dataset)) if idx % 5 == shift]
-
-    # Create the DataLoaders
-    train_loader = DataLoader(Subset(dataset, train_indices), batch_size=1, shuffle=False)
-    validation_loader = DataLoader(Subset(dataset, validation_indices), batch_size=1, shuffle=False)
-
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-                                                                                                      
-    # Init Model
     model = DinoFeatureClassifier().to(device)
-    # binary output true or false
-    criterion = nn.BCELoss()
-    #optimizer
-    optimizer = get_optimizer(model, args.optimizer)
 
-    print(f'Starting ... Optimizer: {args.optimizer}, Shift: {shift}, device: {device}')
+    if args.optimizer_index == 0:
+        optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9)
+    elif args.optimizer_index == 1:
+        optimizer = AdamW(model.parameters(), lr=0.01)
+    elif args.optimizer_index == 2:
+        optimizer = Adam(model.parameters(), lr=0.01)
 
-    # Training and validation loop
-    for epoch in range(num_epochs):
+    criterion = nn.BCEWithLogitsLoss()
 
-        # Train
-        model.train()  
-        for images, labels in train_loader:
-            images = images.to(device)
-            labels = labels.to(device).float().view(-1, 1)
+    dataset = get_data(args.shift, args.data_dir, args.crop_size)
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
-            optimizer.zero_grad() 
-            outputs = model(images) 
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-        # Validate
-        model.eval() 
-        validation_loss = 0
-        correct = 0
-        all_labels = []
-        all_predictions = []
-        with torch.no_grad():
-            for images, labels in validation_loader:
-                images = images.to(device)
-                labels = labels.to(device).float().view(-1, 1)
-
-                outputs = model(images)
-                loss = criterion(outputs, labels)
-                validation_loss += loss.item()
-                _, predicted = torch.max(outputs, 1)
-                correct += (predicted == labels).sum().item()
-
-                all_labels.extend(labels.tolist())
-                all_predictions.extend(predicted.tolist())
-
-        f1 = f1_score(all_labels, all_predictions)
-
-        tot_validation_loss = validation_loss / len(validation_loader)
-
-        if tot_validation_loss  < best_validation_loss:
-           best_validation_loss  = tot_validation_loss
-           torch.save(model.state_dict(), f"vit_opt_{args.optimizer}_shift_{args.shift}.pth")
+    for epoch in range(args.epochs):
+        epoch_loss, f1 = train(model, optimizer, criterion, dataloader, device)
+        logging.info(f'Epoch: {epoch + 1}, Loss: {epoch_loss}, F1: {f1}')
 
 
-        print(f'Epoch {epoch + 1}, Train Loss: {loss.item()}, Validation Loss: {validation_loss / len(validation_loader)}, Validation Accuracy: {correct / len(validation_indices)}, Validation F1 Score: {f1}')
+if __name__ == "__main__":
+    main()
+ 
