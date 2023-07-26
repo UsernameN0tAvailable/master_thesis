11e20e2e7520db5599ccafaa8f621df09e56d695
UsernameN0tAvailable <mattia.pedrazzi@outlook.com>
2023-07-26 15:12:28 +0200

    declaration order

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,59 @@ import os
 import logging
 import json
 
+class HotspotDataset(Dataset):
+    def __init__(self, image_dir: str, splits: List[str], labels: List[int], transform):
+        self.image_dir = image_dir
+        self.splits = splits
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.splits)
+
+    def get_class_weights(self):
+
+        labels_length = len(self.labels)
+
+        positive_count = 0
+
+        for l in self.labels:
+            positive_count += l
+
+        weights = [(labels_length - positive_count) / labels_length, positive_count / labels_length]
+
+        return torch.tensor(weights)
+
+
+    def __getitem__(self, idx):
+        img_name = self.splits[idx]
+        img_path = os.path.join(self.image_dir, f'{img_name}.png')
+        label = self.labels[idx]
+        image = Image.open(img_path)
+        image = self.transform(image)
+        return image, label
+
+class PathsAndLabels():
+    def __init__(self, data_dir: str):
+        self.paths = []
+        self.labels = []
+        self.data_dir = data_dir
+
+    def append(self, split: Dict[str, List[str]]):
+        image_dir = os.path.join(self.data_dir, 'hotspots-png')
+
+        for label in ['0', '1']:
+            for img_name in split[label]:
+                img_path = os.path.join(image_dir, f'{img_name}.png')
+                if os.path.exists(img_path):
+                    self.paths.append(img_name)
+                    self.labels.append(int(label))
+                else:
+                    logging.info(f'Image {img_path} not found.')
+
+    def get_dataset(self, transform) -> HotspotDataset:
+        HotspotDataset(self.image_dir, self.paths, self.labels, transform)
+
 def get_dataloaders(shift, data_dir, crop_size):
     with open(os.path.join(data_dir, 'splits.json'), 'r') as f:
         splits = json.load(f)
@@ -46,60 +99,3 @@ def get_dataloaders(shift, data_dir, crop_size):
                 ]
 
 
-
-
-
-class PathsAndLabels():
-    def __init__(self, data_dir: str):
-        self.paths = []
-        self.labels = []
-        self.data_dir = data_dir
-
-    def append(self, split: Dict[str, List[str]]):
-        image_dir = os.path.join(self.data_dir, 'hotspots-png')
-
-        for label in ['0', '1']:
-            for img_name in split[label]:
-                img_path = os.path.join(image_dir, f'{img_name}.png')
-                if os.path.exists(img_path):
-                    self.paths.append(img_name)
-                    self.labels.append(int(label))
-                else:
-                    logging.info(f'Image {img_path} not found.')
-
-    def get_dataset(self, transform) -> HotspotDataset:
-        HotspotDataset(self.image_dir, self.paths, self.labels, transform)
-
-
-
-class HotspotDataset(Dataset):
-    def __init__(self, image_dir: str, splits: List[str], labels: List[int], transform):
-        self.image_dir = image_dir
-        self.splits = splits
-        self.labels = labels
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.splits)
-
-    def get_class_weights(self):
-
-        labels_length = len(self.labels)
-
-        positive_count = 0
-
-        for l in self.labels:
-            positive_count += l
-
-        weights = [(labels_length - positive_count) / labels_length, positive_count / labels_length]
-
-        return torch.tensor(weights)
-
-
-    def __getitem__(self, idx):
-        img_name = self.splits[idx]
-        img_path = os.path.join(self.image_dir, f'{img_name}.png')
-        label = self.labels[idx]
-        image = Image.open(img_path)
-        image = self.transform(image)
-        return image, label
